7acbb80cf9d1ccba659c49fe1a6a19c69342c404
chany-code <channn926@gmail.com>
2021-04-19 18:26:42 +0900

    initial commit

---
+++ taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package tacos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;//MockMvc를 주입한다 
+	
+	@Test
+	public void testHomePage() throws Exception {
+		mockMvc.perform(get("/"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("home"))
+			.andExpect(content().string(containsString));
+		
+		fail("Not yet implemented");
+	}
+
+}
