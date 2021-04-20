3ff0a4783bdeb46f2a14002c36518ce709e719b5
chany-code <channn926@gmail.com>
2021-04-20 18:57:10 +0900

    Ingredient Domain add

--- taco-cloud/src/test/java/tacos/HomeControllerTest.java
+++ taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -1,6 +1,11 @@
 package tacos;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +21,11 @@ public class HomeControllerTest {
 	@Test
 	public void testHomePage() throws Exception {
 		mockMvc.perform(get("/"))
-			.andExpect(status().isOk())
-			.andExpect(view().name("home"))
-			.andExpect(content().string(containsString));
+			.andExpect(status().isOk())//HTTP 200
+			.andExpect(view().name("home"))//home 뷰가 있어야 
+			.andExpect(content().string(containsString("Welcome to...")));
 		
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 }
